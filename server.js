bd73be25a6c4f70d8602fd42161f29703238d570
Tatyana Binns <maslikova.tatyana@knights.ucf.edu>
2021-10-20 02:05:08 -0400

    Add proof-of concept database code

--- server.js
+++ server.js
@@ -1,11 +1,63 @@
 const express = require('express')
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 
 const app = express()
+
 let port = process.env.PORT;
 if (port == null || port == "")
   port = 8080;
+
+let uri = process.env.MONGODB_URI;
+if (uri == null || uri == "")
+  uri = "mongodb://localhost:27017/rate-my-city";
+
+dbApi = {};
+
+async function dbInit(){
+    await mongoose.connect(uri);
+    const UserProfile = mongoose.model('UserProfile', new mongoose.Schema({
+       firstName: String,
+       lastName : String,
+       email    : String,
+       pwhash   : String,
+       ratings  : [{cityid: String, ratingid: String}]
+    }));
+    const CityData = mongoose.model('CityData', new mongoose.Schema({
+       name    : String,
+       state   : String,
+       country : String,
+       rating  : Number,
+       ratings : [{
+           userid     : String, 
+           rating     : Number,
+           description: String,
+           time       : Date,
+           upvotes: [{userid: String}],
+           comments:[{
+                userid : String,
+                comment: String,
+                time   : Date
+           }]
+        }]
+    }));
+    dbApi.allUsers          = ()     => UserProfile.find();
+    dbApi.userByEmail       = (e)    => UserProfile.find({email: e});
+    dbApi.updateUserByEmail = (e, u) => UserProfile.findOneAndUpdate({email: e}, u, ()=>{});
+    dbApi.createUser  = (f,l,e,pw)   => {
+        const newUser = new UserProfile({
+            firstName: f,
+            lastName : l,
+            email    : e,
+            pwhash   : pw
+        }).save();
+    };
+
+    dbApi.allCities   = ()         => CityData.find();
+}
+dbInit().catch(err => console.log(err));
+
+
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -20,6 +72,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to a simple hello-world application.", additional: "This is additional text."});
 })
+
+
+//*********Example Endpoints, to be deleted*************
+app.get('/mkTestUser', (req, res) => {
+    dbApi.createUser("Robert","Dottingham","billybobjoe@example.com","trololololo");
+    res.send("User Created");
+})
+app.get('/listUsers', (req, res) => {
+    dbApi.allUsers().lean().exec(function (err, users) {
+        return res.send(JSON.stringify(users));
+    });
+})
+app.get('/findByEmail', (req, res) => {
+    var email = "thebigjd@example.com"
+    dbApi.userByEmail(email).lean().exec(function (err, users) {
+        return res.send(JSON.stringify(users));
+    });
+})
+app.get('/editUserByEmail', (req, res) => {
+    var email = "thebigjd@example.com"
+    
+    dbApi.updateUserByEmail(email, {firstName: "Johnny"});
+    
+    res.send("Update Complete");
+})
+//*******************************************************
+
+
+
 
 app.listen(port, () => {
 	  console.log(`Example app listening on port ${port}`)
