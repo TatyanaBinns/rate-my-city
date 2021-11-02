8b0efda9bf1d45e800b1d80f72205101d4bbd8af
nickg2000 <89922962+nickg2000@users.noreply.github.com>
2021-11-02 18:40:27 -0400

    test line 154

--- server.js
+++ server.js
@@ -150,7 +150,8 @@ api.setApp( app, dbApi );
 
 
 app.get('/', (req, res) => {
-    res.json({ message: "Welcome to a simple hello-world application..", additional: "This is additional text."});
+    // res.json({ message: "Welcome to a simple hello-world application..", additional: "This is additional text."});
+    windows.location.href='/src/app.js';
 })
 
 
