dfafd13a5258f0987f2bcadb0bb1510ea5d69fd5
nickg2000 <89922962+nickg2000@users.noreply.github.com>
2021-11-03 00:04:56 -0400

    Add files via upload

--- src_maybeunused/components/TopBorder.js
+++ src_maybeunused/components/TopBorder.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { DropdownButton } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+
 
 function TopBorder()
 {
     return(
       <div className="input-group mt-1 mb-2">
-        <div className="input-group-append">
-          <button className="btn text-white fw-bold btn-outline-dark btn-secondary">Options</button>
-        </div>
+        
+        <DropdownButton className="input-group-append icon-play" id="dropdown-basic-button" size="sm" variant="Secondary" menuVariant="secondary">
+          <Dropdown.Item href="#/action-1">Add City</Dropdown.Item>
+        </DropdownButton>
 
         <input className="form-control text-center fw-bold" placeholder="Welcome: User Registered" />
         <div className="input-group-append">
@@ -21,4 +25,4 @@ function TopBorder()
 
     );
   };
-export default TopBorder;
+export default TopBorder; 
