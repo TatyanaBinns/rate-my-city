b8d20cd88e9cd5efc58f0288e35ec158c6c81208
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-11-08 00:57:34 -0500

    Update api.js

--- api.js
+++ api.js
@@ -17,14 +17,14 @@ exports.setApp = function(app, dbApi)
     dbApi.userByEmail(email).lean().exec(function (err, users) {
       if (users)
       {
-        //bcrypt.compare(password, users.pwhash, function(err, res) {
-          //if (res == true)
-          //{
+        bcrypt.compare(password, users.pwhash, function(err, res) {
+          if (res == true)
+          {
             ret = { id:users._id, firstName:users.firstName, lastName:users.lastName, userName: users.userName, pwhash: users.pwhash, error:''};
             res.status(200).json(ret);
-          //}
+          }
 
-        //});
+        });
         /*try
         {
           const token = require("./createJWT.js");
