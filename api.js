728ed4f0034f7f911596f66c133c24dae758e3fc
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-11-10 00:38:28 -0500

    Update api.js

--- api.js
+++ api.js
@@ -96,7 +96,11 @@ exports.setApp = function(app, dbApi)
               //bcrypt.genSalt(saltRounds, (err, salt) => {
                 //bcrypt.hash(password, salt, (err, hash) => {
                   // Now we can store the password hash in db.
-                  var hashed = bcrypt.hash(password, 10);
+                  var hashed;
+                  bcrypt.hash(password, 10, function(err, hash) {
+                    // Store hash in database here
+                    hashed = hash;
+                  });
                   //dbApi.createUser(firstName, lastName, userName, email, password);
                   ret = {hashed: hashed, error: ""};
                   res.status(200).json(ret);
