ae28034a64cc8c2a3c0ae3e7adb3a8ceabc88005
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-11-14 18:26:19 -0500

    Update api.js

--- api.js
+++ api.js
@@ -170,7 +170,7 @@ exports.setApp = function(app, dbApi)
     var ret;
     const {userId, firstName, lastName, userName, email, password, confirmpassword, jwtToken} = req.body;
 
-    await dbApi.userByUserName(userName).lean().exec(function (err, user) {
+    /*await dbApi.userByUserName(userName).lean().exec(function (err, user) {
       if (user != null && user._id != userId)
       {
         ret = {error: "Username is taken."};
@@ -184,7 +184,7 @@ exports.setApp = function(app, dbApi)
         ret = {error: "Email is being used in another account"};
         return res.status(400).json(ret);
       }
-    });
+    });*/
 
     var regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
 
