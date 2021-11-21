0e2f7ace22d38074bb13ec7a12120be1feaf2a28
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-11-20 19:38:55 -0500

    Update api.js

--- api.js
+++ api.js
@@ -219,12 +219,12 @@ exports.setApp = function(app, dbApi)
 
     //const user = await dbApi.userByUserName(userName);
 
-    dbApi.searchCities(userName, city, state).exec(function(err, result) {
+    try {dbApi.searchCities(userName, city, state).exec(function(err, result) {
       res.json(result)
     })}
     catch (err) {
       res.json({message: err.message})
-    } 
+    }
   });
 
   app.get('/api/listStates', async (req, res, next) =>
