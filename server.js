a1341fec62a676589ddc492e20c3367932d44c6f
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-02 10:42:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -565,8 +565,8 @@ app.get('/mkTestCitys', (req, res) => {
 })
 app.get('/getCityByName', (req, res) => {
     (async() =>
-        res.send(JSON.stringify(await dbApi.cityByName("Los Angeles")))
-        //var city = JSON.stringify(await dbApi.cityByName("Los Angeles"));
+        //res.send(JSON.stringify(await dbApi.cityByName("Los Angeles")))
+        var city = JSON.stringify(await dbApi.cityByName("Los Angeles"));
     )();
 })
 app.get('/addRating', (req, res) => {
