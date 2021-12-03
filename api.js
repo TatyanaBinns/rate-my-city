3ff59fa04b9ec327691386ea516b7254f28f6c28
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-02 22:40:17 -0500

    Update api.js

--- api.js
+++ api.js
@@ -21,7 +21,7 @@ exports.setApp = function(app, dbApi)
     {
       await dbApi.updateByToken(emailToken, {isVerified: true});
       const newUser = await dbApi.userByToken(emailToken);
-      var ret = {isVerified: newUser.isVerified};
+      ret = {isVerified: newUser.isVerified};
       res.json(ret);
     } else {
       {
