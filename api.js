8d8787eba3e1e0b9d7e4c5d750112998c36dc2c6
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-03 23:32:07 -0500

    Update api.js

--- api.js
+++ api.js
@@ -330,8 +330,8 @@ exports.setApp = function(app, dbApi)
     }
 
     (async() => {
-      await dbApi.addRating(email, city, rating, review);
-      error = "";
+      var num = await dbApi.addRating(email, city, rating, review);
+      error = num;
     })();
 
     var refreshedToken = null;
