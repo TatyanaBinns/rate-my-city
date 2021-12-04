87726646a13e9c62b0b9bea879fb1e8202f473e9
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-03 20:23:51 -0500

    testing edit

--- api.js
+++ api.js
@@ -346,14 +346,12 @@ exports.setApp = function(app, dbApi)
     }
 
     (async() => {
-      await dbApi.deleteRating(email, city);
+      //await dbApi.deleteRating(email, city);
       //await dbApi.addRating(email, city, rating, review);
-      //await dbApi.editRating(email, city, rating, review);
+      await dbApi.editRating(email, city, rating, review);
       error = "";
     })();
-  (async() => {
-    await dbApi.addRating(email, city, rating, review);
-    })();
+
     var refreshedToken = null;
 
     try
