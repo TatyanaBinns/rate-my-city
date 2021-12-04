0b92fc54932bee51e802897a970d08bfc71049b6
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-04 06:14:48 -0500

    Update api.js

--- api.js
+++ api.js
@@ -99,11 +99,11 @@ exports.setApp = function(app, dbApi)
 
       var user = await dbApi.createUser(firstName, lastName, userName, email, hashed, emailToken);
 
-      ret = {userId: user._id, firstName: user.firstName, lastName: user.lastName, userName: user.userName, email: user.email, emailToken: user.emailToken, error: ""};
+      /*ret = {userId: user._id, firstName: user.firstName, lastName: user.lastName, userName: user.userName, email: user.email, emailToken: user.emailToken, error: ""};
       res.status(200).json(ret);} catch (err)
       {
         res.json({error: err.message})
-      }
+      }*/
 
     const message =
       {
