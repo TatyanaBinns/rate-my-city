06953c0d6fe3e098e7fd2c983c29c7147da59c15
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-03 22:03:40 -0500

    Update createJWT.js

--- createJWT.js
+++ createJWT.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-exports.createToken = function ( fn, ln, id )
+exports.createToken = function ( fn, ln, id, em )
 {
-    return _createToken( fn, ln, id );
+    return _createToken( fn, ln, id, em );
 }
-_createToken = function ( fn, ln, id )
+_createToken = function ( fn, ln, id, em)
 {
     try
     {
       const expiration = new Date();
-      const user = {userId:id,firstName:fn,lastName:ln};
+      const user = {userId:id,firstName:fn,lastName:ln, email: em};
       //const accessToken =  jwt.sign( user, process.env.ACCESS_TOKEN_SECRET);
       // In order to exoire with a value other than the default, use the
        // following
@@ -50,5 +50,6 @@ exports.refresh = function( token )
   var userId = ud.payload.id;
   var firstName = ud.payload.firstName;
   var lastName = ud.payload.lastName;
-  return _createToken( firstName, lastName, userId );
+  var email = ud.payload.email;
+  return _createToken( firstName, lastName, userId, email );
 }
