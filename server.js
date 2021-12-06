a28b52bffb6064ad2a52e4c95905141baadf470a
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-06 14:57:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -110,7 +110,8 @@ async function dbInit(){
            $and: [
            {"name" : { "$regex": new RegExp(city, 'i' )}},
            {"state" : {"$regex": new RegExp(state, 'i')}}
-          ]}
+         ]},
+         { "$sort" : { "name" : 1, "_id": 1 } }
           },
           {"$unwind" : {"path": "$ratings",
             "preserveNullAndEmptyArrays": true}},
