fe6623b62fcda6a0633d578d0b1096935ffd9d89
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-12-07 15:57:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -439,30 +439,30 @@ async function dbInit(){
         {
           if (city.averageEntertainment == null)
           {
-            city.averageEntertainment = "0";
-            city.averageNature = "0";
-            city.averageCost = "0";
-            city.averageSafety = "0";
-            city.averageCulture = "0";
-            city.averageTransportation = "0";
-            city.averageFood = "0";
+            city.averageEntertainment = "0.0";
+            city.averageNature = "0.0";
+            city.averageCost = "0.0";
+            city.averageSafety = "0.0";
+            city.averageCulture = "0.0";
+            city.averageTransportation = "0.0";
+            city.averageFood = "0.0";
           }
           else {
-            city.averageEntertainment = Math.round(city.averageEntertainment * 10) / 10;
+            /*city.averageEntertainment = Math.round(city.averageEntertainment * 10) / 10;
             city.averageNature = Math.round(city.averageNature * 10) / 10;
             city.averageCost = Math.round(city.averageCost * 10) / 10;
             city.averageSafety = Math.round(city.averageSafety * 10) / 10;
             city.averageCulture = Math.round(city.averageCulture * 10) / 10;
             city.averageTransportation = Math.round(city.averageTransportation * 10) / 10;
-            city.averageFood = Math.round(city.averageFood * 10) / 10;
+            city.averageFood = Math.round(city.averageFood * 10) / 10;*/
 
-            city.averageEntertainment = city.averageEntertainment.toString();
-            city.averageNature =   city.averageNature.toString();
-            city.averageCost = city.averageCost.toString();
-            city.averageSafety = city.averageSafety.toString();
-            city.averageCulture = city.averageCulture.toString();
-            city.averageTransportation = city.averageTransportation.toString();
-            city.averageFood = city.averageFood.toString();
+            city.averageEntertainment = city.averageEntertainment.toFixed(1);
+            city.averageNature =   city.averageNature.toFixed(1);
+            city.averageCost = city.averageCost.toFixed(1);
+            city.averageSafety = city.averageSafety.toFixed(1);
+            city.averageCulture = city.averageCulture.toFixed(1);
+            city.averageTransportation = city.averageTransportation.toFixed(1);
+            city.averageFood = city.averageFood.toFixed(1);
           }
         }
         for (city of res)
